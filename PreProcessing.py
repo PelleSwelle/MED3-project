72ebb98b829b6a0367d8a6839168548a8ae849fe
PelleSwelle <peterdonvang@protonmail.com>
2021-10-08 11:41:57 +0200

    implemented own grayscaling method with pillow

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,16 +1,82 @@
 import cv2 as cv
 import numpy as np
+from copy import copy
+from PIL import Image
+import math
 
 detector = cv.SimpleBlobDetector_create()
 
-def removeBackground(src):
+
+def binarize(src):
     grayScaleImg = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 
     th, otsuThresh = cv.threshold(grayScaleImg, 128, 192, cv.THRESH_OTSU)
     # otsuThreshInverted = cv.bitwise_not(otsuThresh)
-    keypoints = detector.detect(otsuThresh)
+    # keypoints = detector.detect(otsuThresh)
 
-    im_with_keypoints = cv.drawKeypoints(otsuThresh, keypoints, np.array([]), (0, 0, 255),
-                                         cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # im_with_keypoints = cv.drawKeypoints(otsuThresh, keypoints, np.array([]), (0, 0, 255),
+    # cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-    return im_with_keypoints
+    return otsuThresh
+
+def grayScale(src):
+    # load the pixel data from the image
+    pxls = src.load()
+    height, width = src.size
+
+    # make a canvas to hold the new picture
+    img_grayscaled = Image.new(src.mode, src.size)
+
+    for row in range(0, height):
+        for column in range(0, width):
+            # capture the color channels
+            red, blue, green = pxls[row, column]
+            avg = math.floor((red + blue + green) / 3)
+            # fill the canvas with the new values
+            img_grayscaled.putpixel((row, column), (avg, avg, avg))
+
+    result = np.array(img_grayscaled)
+    return result
+
+
+def blur_gaussian(src):
+    kernel = np.array([[1, 2, 1],
+                       [2, 4, 2],
+                       [1, 2, 1]])
+    output = copy(src)
+    # get pixel data
+    # pxls = src.load()
+
+    # Creating coordinates of the pixel (x,y)
+    # coordinates = x, y = 40, 60
+
+    # getting pixel value using getpixel() method
+    # print(pxls.getpixel(coordinates))
+
+    kernelSum = np.sum(kernel)
+    matrix = np.zeros(src.shape)
+    mat = np.matrix(matrix)
+
+    normalized = 0
+    print("kernelSum: ", kernelSum)
+
+    for _y in range(1, src.shape[0] - 1):
+        for _x in range(1, src.shape[1] - 1):  # for every pixel
+            inputSum = 0
+            # for value in range(1):
+            # inputSum += src[_y + color_y - 1, _x + color_x - 1] * kernel[color_y, color_x]
+
+            mat[_y, _x] = inputSum/kernelSum
+            print(mat[_y, _x])
+
+
+                # for color_x in range(1):
+                #     # normalized += inputSum / kernelSum
+            output[_y, _x] = inputSum / kernelSum
+
+    # write matrix to file
+    with open('matrix.txt') as file:
+        for line in mat:
+            # np.savetxt(file, line, fmt='%.2f')
+            pass
+    return output
--- singleImage.py
+++ singleImage.py
@@ -1,23 +1,31 @@
 import cv2 as cv
 import PreProcessing
-
+from PIL import Image
+import math
 import numpy as np
+from copy import copy
 
 imgPath = "./reference1.jpg"
 scaleFactor = 500
+
 initImg = cv.imread(imgPath)
+pilImg = Image.open(imgPath)
+
 img = cv.resize(initImg, (initImg.shape[1] - scaleFactor, initImg.shape[0] - scaleFactor))
 cv.imshow("reference photo", img)
 
-img_Grayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # TODO make our own grayscaler
+img_Grayscale = PreProcessing.grayScale(pilImg)
+# img_gaussianBlur = PreProcessing.blur_gaussian(img_Grayscale)
 
-img_canny = cv.Canny(img_Grayscale, 100, 200)
+img_canny = cv.Canny(img_Grayscale, 100, 200)  # TODO make our own edge detection algorithm
 
-th, img_thresholded = cv.threshold(img_Grayscale, 128, 129, cv.THRESH_BINARY) # TODO make own thresholder
+th, img_thresholded = cv.threshold(img_Grayscale, 128, 129, cv.THRESH_BINARY)  # TODO make own thresholder
+
+
 
 cv.imshow("img_Grayscale", img_Grayscale)
-cv.imshow("img_thresholded", img_canny)
-
+# cv.imshow("img_thresholded", img_canny)
+# cv.imshow("gaussian blur", img_gaussianBlur)
 cv.waitKey(0)
 
 cv.destroyAllWindows()
