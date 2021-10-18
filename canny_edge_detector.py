4c669e6c0e30817e90a489f80345355af7c63c3c
Hpouls <Hpouls20@student.aau.dk>
2021-10-18 14:31:24 +0200

    Canny edge
    Cleaning up the code -- Still not able to run it
    Needs a way to open images

--- canny_edge_detector.py
+++ canny_edge_detector.py
@@ -1,26 +1,18 @@
+# import cv2 as cv
+# import argparse
+# import PreProcessing
+# from PIL import Image
+# from scipy import misc
 from scipy import ndimage
 from scipy.ndimage.filters import convolve
-
-import cv2 as cv
-import argparse
-import PreProcessing
-from PIL import Image
-
-from scipy import misc
 import numpy as np
 
-referenceDir = "./reference/"
 
-SIGN_A = Image.open(referenceDir + "A1008.jpg")
-SIGN_F = Image.open(referenceDir + "F1001.jpg")
-SIGN_P = Image.open(referenceDir + "P1014.jpg")
-
-
-class cannyEdgeDetector:
-    def __init__(self, imgs, sigma=1, kernel_size=5, weak_pixel=75, strong_pixel=255, lowthreshold=0.05,
+class CannyEdgeDetector:
+    def __init__(self, img, sigma=1, kernel_size=5, weak_pixel=75, strong_pixel=255, lowthreshold=0.05,
                  highthreshold=0.15):
-        self.imgs = imgs
-        self.imgs_final = []
+        self.img = img
+        self.img_final = []
         self.img_smoothed = None
         self.gradientMat = None
         self.thetaMat = None
@@ -34,33 +26,36 @@ class cannyEdgeDetector:
         self.highThreshold = highthreshold
         return
 
-    def gaussian_kernel(self, size, sigma=1):
+    @staticmethod
+    def gaussian_kernel(size, sigma=1):
         size = int(size) // 2
         x, y = np.mgrid[-size:size + 1, -size:size + 1]
         normal = 1 / (2.0 * np.pi * sigma ** 2)
         g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2))) * normal
         return g
 
-    def sobel_filters(self, img):
-        Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-        Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+    @staticmethod
+    def sobel_filters(img):
+        kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+        ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
 
-        Ix = ndimage.filters.convolve(img, Kx)
-        Iy = ndimage.filters.convolve(img, Ky)
+        ix = ndimage.filters.convolve(img, kx)
+        iy = ndimage.filters.convolve(img, ky)
 
-        G = np.hypot(Ix, Iy)
+        G = np.hypot(ix, iy)
         G = G / G.max() * 255
-        theta = np.arctan2(Iy, Ix)
-        return (G, theta)
+        theta = np.arctan2(iy, ix)
+        return G, theta
 
-    def non_max_suppression(self, img, D):
-        M, N = img.shape
-        Z = np.zeros((M, N), dtype=np.int32)
-        angle = D * 180. / np.pi
+    @staticmethod
+    def non_max_suppression(img, d):
+        m, n = img.shape
+        z = np.zeros((m, n), dtype=np.int32)
+        angle = d * 180. / np.pi
         angle[angle < 0] += 180
 
-        for i in range(1, M - 1):
-            for j in range(1, N - 1):
+        for i in range(1, m - 1):
+            for j in range(1, n - 1):
                 try:
                     q = 255
                     r = 255
@@ -70,59 +65,54 @@ class cannyEdgeDetector:
                         q = img[i, j + 1]
                         r = img[i, j - 1]
                     # angle 45
-                    elif (22.5 <= angle[i, j] < 67.5):
+                    elif 22.5 <= angle[i, j] < 67.5:
                         q = img[i + 1, j - 1]
                         r = img[i - 1, j + 1]
                     # angle 90
-                    elif (67.5 <= angle[i, j] < 112.5):
+                    elif 67.5 <= angle[i, j] < 112.5:
                         q = img[i + 1, j]
                         r = img[i - 1, j]
                     # angle 135
-                    elif (112.5 <= angle[i, j] < 157.5):
+                    elif 112.5 <= angle[i, j] < 157.5:
                         q = img[i - 1, j - 1]
                         r = img[i + 1, j + 1]
 
                     if (img[i, j] >= q) and (img[i, j] >= r):
-                        Z[i, j] = img[i, j]
+                        z[i, j] = img[i, j]
                     else:
-                        Z[i, j] = 0
-
-
-                except IndexError as e:
+                        z[i, j] = 0
+                except IndexError:
                     pass
-
-        return Z
+        return z
 
     def threshold(self, img):
 
-        highThreshold = img.max() * self.highThreshold;
-        lowThreshold = highThreshold * self.lowThreshold;
+        highThreshold = img.max() * self.highThreshold
+        lowThreshold = highThreshold * self.lowThreshold
 
-        M, N = img.shape
-        res = np.zeros((M, N), dtype=np.int32)
+        m, n = img.shape
+        res = np.zeros((m, n), dtype=np.int32)
 
         weak = np.int32(self.weak_pixel)
         strong = np.int32(self.strong_pixel)
 
         strong_i, strong_j = np.where(img >= highThreshold)
-        zeros_i, zeros_j = np.where(img < lowThreshold)
+        # zeros_i, zeros_j = np.where(img < lowThreshold)
 
         weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
 
         res[strong_i, strong_j] = strong
         res[weak_i, weak_j] = weak
 
-        return (res)
+        return res
 
     def hysteresis(self, img):
-
-        M, N = img.shape
+        m, n = img.shape
         weak = self.weak_pixel
         strong = self.strong_pixel
-
-        for i in range(1, M - 1):
-            for j in range(1, N - 1):
-                if (img[i, j] == weak):
+        for i in range(1, m - 1):
+            for j in range(1, n - 1):
+                if img[i, j] == weak:
                     try:
                         if ((img[i + 1, j - 1] == strong) or (img[i + 1, j] == strong) or (img[i + 1, j + 1] == strong)
                                 or (img[i, j - 1] == strong) or (img[i, j + 1] == strong)
@@ -131,19 +121,18 @@ class cannyEdgeDetector:
                             img[i, j] = strong
                         else:
                             img[i, j] = 0
-                    except IndexError as e:
+                    except IndexError:
                         pass
-
         return img
 
     def detect(self):
-        imgs_final = []
-        for i, img in enumerate(self.imgs):
+        # img_final = []
+        for i, img in enumerate(self.img):
             self.img_smoothed = convolve(img, self.gaussian_kernel(self.kernel_size, self.sigma))
             self.gradientMat, self.thetaMat = self.sobel_filters(self.img_smoothed)
             self.nonMaxImg = self.non_max_suppression(self.gradientMat, self.thetaMat)
             self.thresholdImg = self.threshold(self.nonMaxImg)
             img_final = self.hysteresis(self.thresholdImg)
-            self.imgs_final.append(img_final)
+            self.img_final.append(img_final)
 
-        return self.imgs_final
+        return self.img_final
