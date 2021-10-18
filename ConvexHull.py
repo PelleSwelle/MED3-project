18c72d3b25a0a9f43f9457488821cf33f6302db2
carndt20 <carndt20@student.aau.dk>
2021-10-18 14:59:25 +0200

    Detection

--- ConvexHull.py
+++ ConvexHull.py
@@ -1,11 +1,19 @@
 import numpy as np
 import cv2
 from PIL import Image
+import matplotlib
 
-imgResize = cv2.imread("hand.jpg", 0)
 
+image = cv2.imread("hand.jpg", 0)
+
+
+#Predefining a constant k as the centroid point for the palm
+
+k = 1
+
+#Resize image
 width, height = 300, 300
-hand = cv2.resize(imgResize, (width, height))
+hand = cv2.resize(image, (width, height))
 
 ret, threshold = cv2.threshold(hand, 10, 255, cv2.THRESH_BINARY)
 
@@ -13,6 +21,10 @@ contours, hierachy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX
 
 hull = [cv2.convexHull(c) for c in contours]
 final = cv2.drawContours(hand, hull, -1, (255, 255, 255))
+
+#Finding centroid point of palm
+
+
 
 
 
